94b3313adaefd9e52e65f4858e7f03f8b8ee578d
lisongyuegit <18974849224@163.com>
2019-12-14 21:53:14 +0800

    2019121401

--- src/main/java/com/lsytest/demo/controller/TestModeController.java
+++ src/main/java/com/lsytest/demo/controller/TestModeController.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 //import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -12,14 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestModeController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-//    @Autowired
-//    private JdbcTemplate jdbcTemplate;
-
     @RequestMapping(value = "/testRun",method = RequestMethod.GET)
-    public String testRun(){
-        String result = "测试成功";
+    public String testRun(@RequestParam Map map){
+        String result = "";
         try{
-
+            logger.info("页面输入参数为: " + map);
+            result = "你好,欢迎!";
         }catch(Exception e){
             result = "代码运行错误" + e;
             e.printStackTrace();
@@ -28,14 +29,5 @@ public class TestModeController {
         return result;
 
     }
-
-//    @RequestMapping("/getUsers")
-//    public List<Map<String, Object>> getDbType(){
-//        String sql = "select * from lsy_improt_config";
-//        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
-//        System.out.printf("数据库查询为:"+ JSONObject.toJSONString(list));
-//        return list;
-//    }
-
 
 }
